76bfb633bc9ece79f063752081f80401fd1fbdec
Overlord84 <yuyueugene84@gmail.com>
2015-02-24 10:53:34 +0800

    added admin data in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(id: 1, username: 'overlord', timezone: 'Central Time (US & Canada)', role: 'admin')
